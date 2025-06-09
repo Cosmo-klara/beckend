958d1b566fb359aa884920099909eba2ff859c31
Cosmo Clara <Wincosmo9@outlook.com>
2025-06-10 01:26:59 +0800

    modified:   data/database.sql

--- data/database.sql
+++ data/database.sql
@@ -30,7 +30,7 @@ CREATE TABLE stations (
     capacity INT,                                    -- 容量
     is_open BOOLEAN DEFAULT TRUE,                    -- 是否营业
 
-    FOREIGN KEY (manager_id) REFERENCES station_manager(id)
+    FOREIGN KEY (manager_id) REFERENCES station_managers(id)
 );
 
 CREATE TABLE comments (
